193aaf02eab4e67d9f09419509e5dfcf8550af89
Jonathan <jacharette@icloud.com>
2026-06-09 14:37:17 -0500

    First git commit

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const money = document.getElementById('money');
+    const amountWon = document.getElementById('amount-won');
+    const getTicket = document.getElementById('try-luck');
+
+    let balance = 0;
+
+    // Choose a random number that becomes the prize
+    function getRandomInt() {
+        const weightedRandom = Math.random() ** 5; // Raise to the 5th power so a large win is rare
+        return Math.floor(weightedRandom * (1000 - 10 + 1)) + 10; 
+    }
+
+    // Confetti function so the config isn't pasted everywhere
+    function celebrate() {
+        confetti({
+            velocity: 300,
+            count: 100
+        });
+    }
+
+    // Get a random number, throw confetti if high, and update both texts
+    getTicket.addEventListener('click', () => {
+        let moneyWon = getRandomInt();
+
+        if (moneyWon >= 300) {
+            celebrate();
+        }
+
+        balance = (+balance) + moneyWon
+
+        money.innerHTML = balance.toLocaleString();
+        amountWon.innerHTML = moneyWon.toLocaleString();
+
+        money.classList.add('pop');
+        setTimeout(() => {
+            money.classList.remove('pop');
+        }, 200);        
+    });
+})
